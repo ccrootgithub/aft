1385a67482c722c380acd91ef9427a47abc00cdf
cc <1240988014@qq.com>
2019-05-14 17:16:50 +0800

    88

--- java/src/cc/com/cn/lala.java
+++ java/src/cc/com/cn/lala.java
@@ -4,5 +4,6 @@ public class lala {
     public static void main(String[] args) {
         System.out.println("jjj");
         System.out.println("kkkk");
+        System.out.println("6666");
     }
 }
